bc9eabf1c49c3929983a9c851b593aa5cee173b8
bilal <56475491+Ambush64@users.noreply.github.com>
2023-03-03 13:35:31 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom";
+import ReactQuill, { Quill } from "react-quill-with-table";
+import QuillBetterTable from "quill-better-table";
+import { Parser as HtmlToReactParser } from "html-to-react";
+
+import "./styles.css";
+import "react-quill-with-table/dist/quill.snow.css";
+import "react-quill-with-table/dist/quill.bubble.css";
+import * as QuillTableUI from "quill-table-ui";
+
+var htmlToReactParser = new HtmlToReactParser();
+
+Quill.register("modules/better-table", QuillBetterTable);
+
+const editorModules = {
+  table: false, // disable table module
+  "better-table": {
+    operationMenu: {
+      items: {
+        unmergeCells: {
+          text: "Another unmerge cells name"
+        }
+      }
+    }
+  },
+  keyboard: {
+    bindings: QuillBetterTable.keyboardBindings
+  }
+};
+
+function App() {
+  const editor = useRef();
+  const [text, setText] = useState("");
+  var reactElement = htmlToReactParser.parse(text);
+  useEffect(() => {
+    const editon = editor.current.getEditor();
+    //console.log(editon.getModule("toolbar"));
+    let tableModule = editon.getModule("better-table");
+    tableModule.insertTable(3, 3);
+    console.log(tableModule);
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <ReactQuill
+        ref={editor}
+        value={text}
+        modules={editorModules}
+        onChange={(value) => setText(value)}
+        theme="snow"
+      />
+
+      {reactElement}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
